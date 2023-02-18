fba7a0659583d06be36260078f13805dc7428a16
Vitaly Baev <vitalij.baev.98@bk.ru>
2023-02-18 15:33:59 +0300

    add ci

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -6,7 +6,7 @@ import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
-    return [
+    const plugins = [
         new HTMLWebpackPlugin({
             template: paths.html,
         }),
@@ -23,4 +23,11 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
             openAnalyzer: false,
         }),
     ];
+
+    if (isDev) {
+        plugins.push(new webpack.HotModuleReplacementPlugin());
+        plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }));
+    }
+
+    return plugins;
 }
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,7 +1,9 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
+import { Button } from 'shared/ui/Buttton/Button';
+import { useTranslation } from 'react-i18next';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -10,6 +12,7 @@ interface SidebarProps {
 
 export const Sidebar = ({ className }: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false);
+    const { t } = useTranslation();
 
     const onToggle = () => {
         setCollapsed((prev) => !prev);
@@ -22,13 +25,12 @@ export const Sidebar = ({ className }: SidebarProps) => {
                 classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])
             }
         >
-            <button
+            <Button
                 data-testid="sidebar-toggle"
-                type="button"
                 onClick={onToggle}
             >
-                Toggle
-            </button>
+                {t('Toggle')}
+            </Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher className={cls.lang} />
