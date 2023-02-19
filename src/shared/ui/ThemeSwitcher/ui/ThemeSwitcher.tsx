5c7c9e7f553ece62f2e1a3e23cc633b3fbc5b878
Vitaly Baev <vitalij.baev.98@bk.ru>
2023-02-19 10:18:43 +0300

    upgrade sidebar

--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,7 +1,7 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import React from 'react';
 import { Theme, useTheme } from 'app/providers/ThemeProvider';
-import { Button, ThemeButton } from 'shared/ui/Buttton/Button';
+import { Button, ButtonTheme } from 'shared/ui/Buttton/Button';
 import LightIcon from 'shared/assets/icons/theme-light.svg';
 import DarkIcon from 'shared/assets/icons/theme-dark.svg';
 
@@ -14,7 +14,7 @@ export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
 
     return (
         <Button
-            theme={ThemeButton.CLEAR}
+            theme={ButtonTheme.CLEAR}
             className={classNames('', {}, [className])}
             onClick={toggleTheme}
         >
