9ca967a7fcc2dc3d0b17df37161fec17f403faf6
Vitaly Baev <vitalij.baev.98@bk.ru>
2023-02-23 18:46:15 +0300

    add redux

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,7 +1,6 @@
 import { BugButton } from 'app/providers/ErrorBoundary';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-
 const MainPage = () => {
     const { t } = useTranslation('main');
 
