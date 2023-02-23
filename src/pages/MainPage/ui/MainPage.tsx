64951583c58647e71111db0db2edd30b07190411
Vitaly Baev <vitalij.baev.98@bk.ru>
2023-02-23 18:51:56 +0300

    refactoring lint

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,6 +1,7 @@
 import { BugButton } from 'app/providers/ErrorBoundary';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+
 const MainPage = () => {
     const { t } = useTranslation('main');
 
